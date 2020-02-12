ea647d2ff6c6183c26c4f514d2fb9158e7426881
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 16:45:13 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,22 +9,45 @@ class Artist
 
   def save
     @@all << self
-    @@all.uniq!
-    #binding.pry
   end
 
   def self.all
     @@all
   end
 
-  def add_song(name)
-    new_song = Song.new(name)
-    new_song.artist = self
-  end
-
   def songs
     Song.all.select {|song| song.artist == self}
   end
 
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name, genre = nil)
+     new_song = Song.new(name, genre)
+     add_song(new_song)
+   end
+
+  def self.song_count
+    Song.all.length
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|artist| artist.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) ? find_by_name(name) : Artist.new(name)
+  end
+
+  def print_songs
+    song_names = []
+    Song.all.each do |song|
+      if song.artist == self
+        song_names << song.name
+      end
+    end
+    puts song_names
+  end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
+require "pry"
 class Song
-  attr_accessor :title, :artist, :genre
+  attr_accessor :artist,:name, :genre
   @@all = []
-  def initialize(name, genre = nil)
-    @name = name
+  def initialize(song, genre = nil)
+    @name = song
     @genre = genre
+    save
   end
 
   def save
@@ -14,7 +16,26 @@ class Song
     @@all
   end
 
-  def artist
-    @artist
+
+  def artist_name
+    answer = nil
+    if self.artist
+    answer = self.artist.name
+    end
+    answer
   end
+
+  def self.new_by_filename(filename)
+    parts_of_filename =[]
+    new_song = nil
+    parts_of_filename = filename.split(" - ")
+    new_song = Song.new(parts_of_filename[1], parts_of_filename[2])
+    new_song.artist_name = parts_of_filename[0]
+    new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+  end
+
 end
