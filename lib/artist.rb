e68c4fb94856aa149369c1a70911ef1f1da9c373
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-17 23:58:47 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,49 +1,34 @@
 require 'pry'
-
 class Artist
-
-  attr_accessor :name, :songs
-  
   @@all = []
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
-    @songs = []
     @@all << self 
   end
 
-  def add_song(song)
-    @songs << song
-  end
-  
   def self.all
     @@all
   end
 
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
   def self.find_or_create_by_name(name)
-    if self.find(name)
-       self.find(name)
-    else
-      self.create(name)
-    end
+    self.find(name) ? self.find(name) : self.new(name)
   end
 
   def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
+    self.all.find {|artist| artist.name == name }
   end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
-
 
   def print_songs
-    @songs.each {|song| puts song.name}
+    songs.each {|song| puts song.name}
   end
-  
-
 end
