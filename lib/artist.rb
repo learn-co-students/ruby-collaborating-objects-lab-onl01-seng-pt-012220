b04921cbae23c66c1fbc6079b6cef9b1350c3137
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-21 21:53:08 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+  attr_accessor :name, :songs
+    
+    @@all = []
+    
+  def initialize (name)
+    @name = name
+    @@all << self
+    @songs = []
+  end 
+  
+  def self.all
+    @@all 
+  end  
+  
+  def add_song (song)
+    @songs << song
+  end
+  
+  def self.find_or_create_by_name(name)
+   find_or_create_name = @@all.find do |artist| artist.name == name 
+   end  
+   if find_or_create_name == nil
+     self.new(name)
+   else
+    find_or_create_name
+   end
+  end
+  
+  def print_songs
+   puts @songs.collect {|song| song.name}
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+ attr_accessor :name, :artist
+ @@all = []
+ def initialize (name)
+  @name = name
+  @@all << self
+ end
+ 
+ def self.all 
+  @@all
+ end
+ 
+ def self.new_by_filename(file_name)
+  song = self.new(file_name.split(" - ")[1])
+   song.artist_name = (file_name.split(" - ")[0])
+   song
+ end
+
+ def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end 
+end
