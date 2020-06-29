e7beb26ba51b81d99d4c92cc9593e1988138f3cb
sanazaidichi <sana.rizvi.5@gmail.com>
2020-06-29 16:40:05 -0500

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class MP3Importer 
+attr_reader :path
+
+def initialize(path)
+    @path = path
+end 
+
+def files 
+    files = []
+    Dir.entries(path).delete_if{|filename| [".",".."].include? filename}
+    end 
+
+
+def import 
+    self.files.each do |filename|
+        Song.new_by_filename(filename)
+    end
+end 
+
+end 
